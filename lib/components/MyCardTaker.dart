81e9abad18f21c0263f657b0d606db20cdfbee2d
buildsmile <you@example.com>
2022-10-23 22:51:16 +0700

    MyCardTaker.dart

---
+++ lib/components/MyCardTaker.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyCardTaker extends StatelessWidget {
+  const MyCardTaker({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 80,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text("12/12/2022"),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("สุพจน์ ใจร่ม"),
+              Text("8 ชั่วโมง / 2500บาท"),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('รอการตอบรับ'),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('ยกเลิก'),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/client/myCaretaker.dart
+++ lib/pages/client/myCaretaker.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../components/MyCardTaker.dart';
+
 class MyCareTaker extends StatefulWidget {
   const MyCareTaker({Key? key}) : super(key: key);
 
@@ -13,35 +15,7 @@ class _MyCareTakerState extends State<MyCareTaker> {
     return Scaffold(
         body: ListView(
       children: [
-        ListTile(
-          leading: Text('12/12/2022'),
-          title: Text('สุพจน์ ใจร่ม'),
-          subtitle: Text('8ชั่วโมง/2500 บาท'),
-          trailing: Column(
-            children: [
-              Text('รอการตอบรับ'),
-              TextButton(
-                onPressed: () {},
-                child: Text("ยกเลิก"),
-              ),
-            ],
-          ),
-        ),
-        ListTile(
-          leading: Text('12/12/2022'),
-          title: Text('สุพจน์ ใจร่ม'),
-          subtitle: Text('8ชั่วโมง/2500 บาท'),
-          trailing: Text('กำลังดำเนินการ'),
-        ),
-        ListTile(
-          leading: Text('10/12/2022'),
-          title: Text('สุขใจ ใจร่ม'),
-          subtitle: Text('8ชั่วโมง/2500 บาท'),
-          trailing: ElevatedButton(
-            onPressed: () {},
-            child: Text("เสร็จสิ้น"),
-          ),
-        ),
+        MyCardTaker(),
       ],
     ));
   }
