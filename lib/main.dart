66e24521078f25e1bd3a8b8d83c5df0e37a3fd64
buildsmile <you@example.com>
2022-11-16 23:22:16 +0700

    initial news

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:take_ama/models/UserLogin.dart';
+import 'package:take_ama/pages/admin/addNews.dart';
 import 'package:take_ama/pages/admin/editDetail.dart';
 import 'package:take_ama/pages/admin/news.dart';
 import 'package:take_ama/pages/admin/user.dart';
@@ -23,16 +24,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        "/register": (context) => RegisterPage(),
-        "/caretaker": (context) => CaretakerCardPage(),
-        "/caretakerDetail": (context) => CaretakerDetailPage(),
-        "/client-home": (context) => ClientHome(),
-        "/caretaker-home": (context) => CareTakerHome(),
-        "/login": (context) => LoginPage(),
-        "/user-admin": (context) => UserPage(),
-        "/detail": (context) => EditDetailPage(),
-        "/menu-admin": (context) => MenuPage(),
-        "/news-admin": (context) => NewsPage()
+        "/register": (context) => const RegisterPage(),
+        "/caretaker": (context) => const CaretakerCardPage(),
+        "/caretakerDetail": (context) => const CaretakerDetailPage(),
+        "/client-home": (context) => const ClientHome(),
+        "/caretaker-home": (context) => const CareTakerHome(),
+        "/login": (context) => const LoginPage(),
+        "/user-admin": (context) => const UserPage(),
+        "/detail": (context) => const EditDetailPage(),
+        "/menu-admin": (context) => const MenuPage(),
+        "/news-admin": (context) => const NewsPage(),
+        "/add-news": (context) => const AddNews()
       },
       home: FutureBuilder(
         future: StorageLocal.getUser(),
